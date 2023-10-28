ab9191b537e9307611cf0b0955446122d9f4b674
Ashwin-Selvaraj <ashwin240899@gmail.com>
2023-10-28 23:58:24 +0530

    Added Palindrome.sol

--- Contest - 8/palindrome.sol
+++ Contest - 8/palindrome.sol
@@ -6,7 +6,6 @@ contract PalindromeChecker {
     //To check if a given string is palindrome or not
     function isPalindrome(string memory _str)external pure returns (bool){
         bytes memory str=bytes(_str); 
-
         uint len = str.length;
         
     }
