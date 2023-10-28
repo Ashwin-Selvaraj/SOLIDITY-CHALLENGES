801c3f8694ce7e4d8c53470d1cc9705c92703751
Ashwin-Selvaraj <ashwin240899@gmail.com>
2023-10-28 23:28:48 +0530

    Added Palindrome.sol

---
+++ Contest - 8/palindrome.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract PalindromeChecker {
+    
+    //To check if a given string is palindrome or not
+    function isPalindrome(string memory _str)external pure returns (bool){
+        bytes memory str=bytes(_str);
+        uint len = str.length;
+        
+    }
+}
